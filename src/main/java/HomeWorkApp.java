6e331ccb4bb017ea086aeff5e3280eb6f5f2b1e1
Nata Khodasevich <you@example.com>
2023-08-29 12:13:37 +0300

    added a new task with fruits

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,11 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void  printThreeWords(){
+        System.out.println("orange");
+        System.out.println("banana");
+        System.out.println("apple");
+    }
+}
