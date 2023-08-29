db247253af68a7d2238816c1161240c507bc6851
Nata Khodasevich <you@example.com>
2023-08-29 16:24:04 +0300

    added 3 more tasks so the lesson1 is completed

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,6 +1,6 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();
+        compareNumbers();
     }
 
     public static void  printThreeWords(){
@@ -8,4 +8,36 @@ public class HomeWorkApp {
         System.out.println("banana");
         System.out.println("apple");
     }
+
+    public static void checkSumSign(){
+        int a=-10;
+        int b=25;
+        if (a+b>=0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor(){
+        int value=-110;
+
+        if(value <= 0){
+            System.out.println("Красный");
+        }
+         else if (value >0 && value <= 100){
+            System.out.println("Желтый");
+    } else{
+            System.out.println("Зеленый");
+        }
+}
+    public static void compareNumbers(){
+        int a=5;
+        int b=10;
+        if(a>=b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+    }
+
 }
